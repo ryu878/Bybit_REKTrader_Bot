8cca2a01604f68c535b3ea604c3cd25c94b0f497
Dmitry <ryu8777@gmail.com>
2025-01-28 14:21:52 +0300

    update rektrader.py

---
+++ rektrader.py
@@ -0,0 +1,12 @@
+# Bybit REKTrader Bot (C) 2025 Ryan Hayabusa
+#
+# Github: https://github.com/ryu878
+#
+# Discord: https://discord.gg/zSw58e9Uvf
+#
+# Join Bybit and receive up to $6,045 in Bonuses: https://www.bybit.com/invite?ref=P11NJW
+#
+# Web: https://aadresearch.xyz
+#
+#######################################################################################################
+
